c881e9f115c2a15371e69e20737643f4b7184218
Toee Thiha Kyaw <toeethihakyaw@gmail.com>
2026-03-31 22:06:33 +0630

    style(ui): refine layout and visual consistency.

--- public/script.js
+++ public/script.js
@@ -1,118 +1,225 @@
 const socket = io();
-let username = document.querySelector(".setName");
-let firstBox = document.querySelector(".alert");
-let info = document.querySelector("#username");
-let list = document.querySelector("#list");
+const username = document.querySelector("#set-name");
+const promptNameBox = document.querySelector("#prompt-name-box");
+const info = document.querySelector("#username");
+const list = document.querySelector("#list");
+const receiverInfo = document.querySelector("#receiver");
+const auth = document.querySelector("#auth");
+const inputBox = document.querySelector("#input-box");
+const sideBar = document.querySelector("#side-bar");
+const backButton = document.querySelector("#back-button");
+let messageBox = document.querySelector("#message-box");
 
-let receiverID;
-let messageHistory = [];
-let msgBox = document.querySelector(".msg-text");
-
-function start() {
-  let uname = username.value.trim();
-  if (uname) {
-    firstBox.classList.add("hide");
-
-    //match name and id
-    socket.emit("createPairOfNameAndId", uname);
-    socket.on("getPairOfNameAndId", (user) => {
-      info.classList.add(user.id);
-      info.innerText = user.name;
-      socket.on("getOnlineUsers", (online) => {
-        let tmp = "";
-        online
-          .filter((v) => {
-            return v.id != user.id;
-          })
-          .forEach((v) => {
-            tmp += `
-                    <tr>
-                        <td>
-                            <input type="radio" name="user" value="${v.id}" onchange="choose(this)"><b>${v.name}</b>
-                        </td>
-                    </tr>
-                    `;
-          });
-        list.innerHTML = tmp;
-        receiverID = "";
-        msgBox.innerHTML = "";
-      });
-    });
+class User {
+  setId(id) {
+    this.id = id;
+  }
+  setName(name) {
+    this.name = name;
+  }
+  getId() {
+    return this.id;
+  }
+  getName() {
+    return this.name;
   }
 }
 
+class Message {
+  constructor(sender, receiver, message) {
+    this.sender = sender;
+    this.receiver = receiver;
+    this.message = message;
+  }
+}
+
+const owner = new User();
+const receiver = new User();
+
+let messageHistory = [];
+
+function renderUserName(id, name, checked) {
+  return `<tr class="font-semibold">
+            <td class="border-b border-gray-300">
+              <input type="radio" name="user" value="${id}" onclick="choose('${id}', '${name}')" 
+                id="${id}"
+                ${checked && "checked"}
+                class="[&:checked+label>div]:bg-gray-300 hidden">
+              <label for="${id}" >
+                <div class="p-2">
+                <div class="break-all">${name}</div>
+                <div>ID : ${id.slice(0, 5)}*****</div>
+                </div>
+              </label>
+             </td>
+          </tr>`;
+}
+
+function resetMessageBox() {
+  receiver.setId(null);
+  receiver.setName(null);
+  messageBox.innerHTML = "";
+  receiverInfo.innerText = "?";
+  auth.scrollLeft = 0;
+}
+
+function start() {
+  // check username is empty or not
+  if (username.value.trim()) {
+    promptNameBox.classList.add("hidden");
+    auth.classList.remove("hidden");
+
+    // register user account
+    socket.emit("createPairOfNameAndId", username.value.trim());
+  }
+}
+
+socket.on("getPairOfNameAndId", (user) => {
+  info.classList.add(user.id);
+  info.innerText = `Hello, ${user.name}`;
+  owner.setId(user.id);
+  owner.setName(user.name);
+});
+
+// list.innerHTML = Array.from({ length: 20 }, (_, index) => {
+//   return renderUserName(index, `Name ${index}`, false);
+// }).join("");
+
+socket.on("getOnlineUsers", (users) => {
+  list.innerHTML = users
+    .filter((v) => v.id != owner.getId())
+    .map((v) => renderUserName(v.id, v.name, v.id == receiver.getId()))
+    .join("");
+
+  const isUserOnline = users.find((user) => user.id == receiver.getId());
+  if (!isUserOnline) resetMessageBox();
+});
+
 document.forms[0].onsubmit = function () {
-  if (receiverID) {
-    let text = document.forms[0].children[0];
-    if (text.value.trim()) {
-      let msgObject = {
-        sender: info.classList[0],
-        receiver: receiverID,
-        message: text.value.trim(),
-      };
-      messageHistory.push(msgObject);
-      socket.emit("sendMessage", msgObject);
-      let box = document.createElement("DIV");
-      box.classList.add("y");
-      let msg = document.createElement("DIV");
-      msg.classList.add("you");
-      let txt = document.createTextNode(msgObject.message);
-      msg.appendChild(txt);
-      box.appendChild(msg);
-      msgBox.appendChild(box);
-      msgBox.scrollTop = msgBox.scrollHeight;
-      text.value = "";
+  if (receiver.getId()) {
+    if (inputBox.value.trim()) {
+      let message = new Message(
+        owner.getId(),
+        receiver.getId(),
+        inputBox.value.trim()
+      );
+      messageHistory.push(message);
+      socket.emit("sendMessage", message);
+
+      renderSentMessage(message.message);
+      scrollMessageToBottom();
+      resetInputBox();
     }
   } else {
     alert("You haven't selected anyone yet.");
   }
   return false;
 };
+
+function scrollMessageToBottom() {
+  messageBox.scrollTop = messageBox.scrollHeight;
+}
+function resetInputBox() {
+  inputBox.value = "";
+}
+
+function renderSentMessage(message) {
+  let outerBoxContainer = document.createElement("div");
+  let messageBoxContainer = document.createElement("div");
+
+  outerBoxContainer.classList.add("flex", "justify-end", "py-2");
+  messageBoxContainer.classList.add(
+    "bg-gray-200",
+    "font-semibold",
+    "m-1",
+    "px-4",
+    "py-2",
+    "break-all",
+    "max-w-4/5",
+    "w-fit",
+    "relative",
+    "after:absolute",
+    "after:content-['']",
+    "after:border-x-5",
+    "after:border-y-7",
+    "after:border-transparent",
+    "after:border-t-gray-200",
+    "after:top-full",
+    "after:right-2"
+  );
+
+  let messageText = document.createTextNode(message);
+  messageBoxContainer.appendChild(messageText);
+  outerBoxContainer.appendChild(messageBoxContainer);
+  messageBox.appendChild(outerBoxContainer);
+}
+
 socket.on("getMessage", (data, senderName) => {
   messageHistory.push(data);
 
-  if (data.sender == receiverID) {
-    let box = document.createElement("DIV");
-    box.classList.add("o");
-    let msg = document.createElement("DIV");
-    msg.classList.add("other");
-    let txt = document.createTextNode(data.message);
-    msg.appendChild(txt);
-    box.appendChild(msg);
-    msgBox.appendChild(box);
-    msgBox.scrollTop = msgBox.scrollHeight;
+  if (data.sender == receiver.getId()) {
+    renderReceivedMessage(data.message);
+    scrollMessageToBottom();
   } else {
     alert("Got a message from " + senderName);
   }
 });
 
-function choose(id) {
-  receiverID = id.value;
-  socket.emit("viewMessage", receiverID, info.classList[0]);
+function renderReceivedMessage(message) {
+  let outerBoxContainer = document.createElement("div");
+  let messageBoxContainer = document.createElement("div");
+
+  outerBoxContainer.classList.add("flex", "justify-start", "py-2");
+  messageBoxContainer.classList.add(
+    "bg-black",
+    "text-white",
+    "font-semibold",
+    "m-1",
+    "px-4",
+    "py-2",
+    "break-all",
+    "max-w-4/5",
+    "w-fit",
+    "relative",
+    "after:absolute",
+    "after:content-['']",
+    "after:border-x-5",
+    "after:border-y-7",
+    "after:border-transparent",
+    "after:border-t-black",
+    "after:top-full",
+    "after:left-2"
+  );
+  let messageText = document.createTextNode(message);
+  messageBoxContainer.appendChild(messageText);
+  outerBoxContainer.appendChild(messageBoxContainer);
+  messageBox.appendChild(outerBoxContainer);
 }
+
+function choose(id, name) {
+  auth.scrollLeft = auth.scrollWidth;
+  receiver.setId(id);
+  receiver.setName(name);
+  receiverInfo.innerText = `${receiver.getName()}`;
+  socket.emit("viewMessage", receiver.getId(), owner.getId());
+}
+
+backButton.onclick = resetMessageBox;
+
 socket.on("viewMessage", (senderID, receiverID) => {
-  msgBox.innerHTML = "";
-  let tmp = "";
-  let conv = messageHistory.filter((v) => {
+  messageBox.innerHTML = "";
+  let conersation = messageHistory.filter((v) => {
     return (
       (v.sender == senderID && v.receiver == receiverID) ||
       (v.sender == receiverID && v.receiver == senderID)
     );
   });
-  conv.forEach((v) => {
-    let box = document.createElement("DIV");
-    let msg = document.createElement("DIV");
-    if (v.sender == receiverID) {
-      box.classList.add("o");
-      msg.classList.add("other");
+  conersation.forEach((data) => {
+    if (data.sender == receiverID) {
+      renderReceivedMessage(data.message);
     } else {
-      box.classList.add("y");
-      msg.classList.add("you");
+      renderSentMessage(data.message);
     }
-    let txt = document.createTextNode(v.message);
-    msg.appendChild(txt);
-    box.appendChild(msg);
-    msgBox.appendChild(box);
-    msgBox.scrollTop = msgBox.scrollHeight;
+    scrollMessageToBottom();
   });
 });
